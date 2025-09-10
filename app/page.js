4f3d8f6a8137107196513808f5d79ecbcfd39845
Khamartaj <kkalegar25@gmail.com>
2025-09-10 18:24:37 +0530

    Header component created successfully

--- app/page.js
+++ app/page.js
@@ -4,12 +4,7 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <>
-      <div>
-         <h2 className='text-yellow-400'>Main component</h2> 
-      </div> <br/> <br/>
-      <div>
-        <Button>Subscribe</Button>
-      </div>
+      
 
   </>
   );
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,111 @@
-import React from 'react'
+import React from "react";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import {
+  ChevronDown,
+  FileText,
+  GraduationCap,
+  LayoutDashboard,
+  PenBox,
+  StarIcon,
+} from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
+      <nav className="container mx-auto flex items-center justify-between  px-4 h-16 ">
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="Logo for carreer kit"
+            width={120}
+            height={40}
+            priority
+            style={{ height: "auto", width: "auto" }} // ✅ maintains aspect ratio
+            className="py-1 object-contain"
+          />
+        </Link>
+        <div className="flex item-center space-x-2 md:space-x-4">
+          <SignedIn>
+            <Link href={"/dashboard"}>
+              <Button variant="outline">
+                <LayoutDashboard className="h-4 w-4" />
+                <span className="hidden md:block">Industry Insights</span>
+              </Button>
+            </Link>
 
-export default Header
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Button>
+                  <StarIcon className="h-4 w-4" />
+                  <span className="hidden md:block">Growth Tools</span>
+                  <ChevronDown className="h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>
+                  <Link href={"/resume"} className="flex items-center gap-2">
+                    <FileText className="h-4 w-4" />
+                    <span>Build Resume</span>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link
+                    href={"/ai-cover-letter"}
+                    className="flex items-center gap-2"
+                  >
+                    <PenBox className="h-4 w-4" />
+                    <span>Cover Letter</span>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href={"/interview"} className="flex items-center gap-2">
+                    <GraduationCap className="h-4 w-4" />
+                    <span>Interview Prep</span>
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton>
+              <Button variant="outline">Sign In</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                  userButtonPopoverCard: "shadow-xl",
+                  userPreviewMainIdentifier: "font-semibold",
+                },
+              }}
+              afterSignOutUrl="/"
+            />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
